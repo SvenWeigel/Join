a0dccb706fa5ed058424848e5e5b33c769c9b5be
Sven <sven.wgl@gmail.com>
2025-12-02 23:00:39 +0100

    add hrefs to summary page btns

---
+++ scripts/summary.js
@@ -0,0 +1,23 @@
+document.getElementById("todo-div").addEventListener("click", function() {
+    window.location.href = "/html/board.html";
+});
+
+document.getElementById("done-div").addEventListener("click", function() {
+    window.location.href = "/html/board.html";
+});
+
+document.getElementById("priority-date-div").addEventListener("click", function() {
+    window.location.href = "/html/board.html";
+});
+
+document.getElementById("tasks-in-board-div").addEventListener("click", function() {
+    window.location.href = "/html/board.html";
+});
+
+document.getElementById("tasks-in-progress-div").addEventListener("click", function() {
+    window.location.href = "/html/board.html";
+});
+
+document.getElementById("awaiting-feedback-div").addEventListener("click", function() {
+    window.location.href = "/html/board.html";
+});
